ac25a69472c0a1f8cdda506fbb5c6a55fa55a4c7
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2024-05-19 12:30:14 -0500

    íconos para la biblioteca de Font Awesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBank, faBuilding, faPencilAlt, faPhone, faPlus, faTag, faTrash, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { createApp } from 'vue';
+
+import App from './App.vue';
+import router from './router';
+
+// íconos para la biblioteca de Font Awesome
+library.add(faBank);
+library.add(faBuilding);
+library.add(faPencilAlt);
+library.add(faPhone);
+library.add(faPlus);
+library.add(faTag);
+library.add(faTrash);
+library.add(faUser);
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.use(router).mount('#app');
