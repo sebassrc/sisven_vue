a0a4fce2aa1fa737f1f32d8f742e9dcab0ff57bb
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2024-05-19 12:46:29 -0500

    new categoria

--- src/components/Categoria/NewCategoria.vue
+++ src/components/Categoria/NewCategoria.vue
@@ -1,92 +1,75 @@
 <template>
-  <div class="container text-start">
-    <h1 class="text-primary fw-bold"> Editar Categoría</h1>
-    <div class="card">
-      <div class="card-header fw-bold">
-        Categoría
-      </div>
-      <div class="card-body">
-        <form @submit.prevent="updateCategoria">
-          
-          <div class="row mb-3">
-            <label for="categoria_id" class="form-label">ID</label>
-            <div class="input-group">
-              <div class="input-group-text"> <font-awesome-icon icon="tag" /></div>
-              <input type="text" class="form-control" id="categoria_id" placeholder="ID categoría" disabled 
-              v-model='categoria.id'
-              >
+    <div class="container text-start">
+      <h1 class="text-primary fw-bold"> Nueva Categoría</h1>
+      <div class="card">
+        <div class="card-header fw-bold">
+          Categoría
+        </div>
+        <div class="card-body">
+          <form @submit.prevent="saveCategoria">
+            
+            <div class="row mb-3">
+                <label for="name" class="form-label">Nombre :</label>
+              <div class="input-group">
+                <div class="input-group-text"> <font-awesome-icon icon="bookmark" /></div>
+                <input type="text" class="form-control" id="name" placeholder="Nombre de la categoría"
+                 v-model="categoria.name"
+                 >
+              </div>
             </div>
-          </div>
-         
-          <div class="row mb-3">
-              <label for="name" class="form-label">Nombre :</label>
-            <div class="input-group">
-              <div class="input-group-text"> <font-awesome-icon icon="bookmark" /></div>
-              <input type="text" class="form-control" id="name" placeholder="Nombre de la categoría"
-               v-model="categoria.name"
-               >
+  
+            <div class="row mb-3">
+                <label for="description" class="form-label">Descripción :</label>
+              <div class="input-group">
+                <div class="input-group-text"> <font-awesome-icon icon="align-left" /></div>
+                <textarea class="form-control" id="description" placeholder="Descripción de la categoría"
+                 v-model="categoria.description"
+                 ></textarea>
+              </div>
             </div>
-          </div>
-
-          <div class="row mb-3">
-              <label for="description" class="form-label">Descripción :</label>
-            <div class="input-group">
-              <div class="input-group-text"> <font-awesome-icon icon="align-left" /></div>
-              <textarea class="form-control" id="description" placeholder="Descripción de la categoría"
-               v-model="categoria.description"
-               ></textarea>
-            </div>
-          </div>
-
-          <button class="btn btn-primary" type="submit">Actualizar</button>
-          <button class="btn btn-secondary mx-2" @click="cancelar">Cancelar</button>
-        </form>
+  
+            <button class="btn btn-primary" type="submit">Guardar</button>
+            <button class="btn btn-secondary mx-2" @click="cancelar">Cancelar</button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-</template>
-
-<script>
-import axios from 'axios';
-import Swal from 'sweetalert2';
-
-export default {
-  name: 'EditarCategoria',
-  data() {
-    return {
-      categoria: {
-        id: 0,
-        name: '',
-        description: ''
-      }
-    };
-  },
-  methods: {
-    cancelar() {
-      this.$router.push({ name: 'categories' });
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  import Swal from 'sweetalert2';
+  
+  export default {
+    name: 'NewCategoria',
+    data() {
+      return {
+        categoria: {
+          name: '',
+          description: ''
+        }
+      };
     },
-
-    async updateCategoria() {
-      const res = await axios.put(`http://127.0.0.1:8000/api/categories/${this.categoria.id}`, this.categoria);
-      
-      if (res.status === 200) {
+    methods: {
+      cancelar() {
         this.$router.push({ name: 'categories' });
-        Swal.fire({
-          position: 'top-end',
-          icon: 'success',
-          title: 'Categoría ha sido actualizada',
-          showConfirmButton: false,
-          timer: 2000
-        });
+      },
+  
+      async saveCategoria() {
+        const res = await axios.post(`http://127.0.0.1:8000/api/categories`, this.categoria);
+        
+        if (res.status === 200) {
+          this.$router.push({ name: 'categories' });
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Categoría ha sido guardada',
+            showConfirmButton: false,
+            timer: 2000
+          });
+        }
       }
     }
-  },
-  mounted() {
-    this.categoria.id = this.$route.params.id;
-    axios.get(`http://127.0.0.1:8000/api/categories/${this.categoria.id}`)
-      .then(response => {
-        this.categoria = response.data.category;
-      });
-  }
-};
-</script>
+  };
+  </script>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import AboutView from '../views/AboutView.vue';
+import Categories from '../views/Categories.vue';
+
+import EditarCategoria from '../components/Categoria/EditarCategoria.vue';
+import NewCategoria from '../components/Categoria/NewCategoria.vue';
 
 const routes = [
   {
@@ -14,12 +19,27 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    component: Categories
+  },
+  {
+    path: '/editar-categoria/:id',
+    name: 'EditarCategoria',
+    component: EditarCategoria
+  },
+  {
+    path: '/add-categoria/',
+    name: 'NewCategoria',
+    component: NewCategoria
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
