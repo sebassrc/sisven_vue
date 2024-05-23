2daec1feba74f95a1a2fae20fc5413181b6e2ffe
Emmanuel Valencia <emmanuelvalencia479@gmail.com>
2024-05-22 20:39:55 -0500

    Vista principal en Vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import Detalles from '../views/Detalles.vue';
+
 const routes = [
   {
     path: '/',
@@ -14,7 +16,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/detalles',
+    name: 'Detalles',
+    component: Detalles
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/Detalles.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <h1>Listado de Detalles</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Invoice</th>
+                    <th scope="col">Product</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(detalle, index) in detalles" :key="index">
+                    <th scope="row">{{ index + 1 }}</th>
+                    <td>{{ detalle.invoice_id }}</td>
+                    <td>{{ detalle.product_id }}</td>
+                    <td>{{ detalle.quantity }}</td>
+                    <td>{{ detalle.price }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Detalle',
+    data() {
+        return {
+            detalles: [] // Use "detalles" instead of "Detalles"
+        };
+    },
+    mounted() {
+        axios
+            .get('http://127.0.0.1:8000/api/detalles')
+            .then(response => {
+                this.detalles = response.data.detalles;
+            });
+    }
+};
+</script>
+
+<style scoped>
+/* Add some styles if needed */
+</style>
